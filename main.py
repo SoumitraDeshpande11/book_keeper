f61d254d3a17fec659c7d78d6ed51edd4b5a4fd2
Soumitra <soumitradesh11@gmail.com>
2024-11-07 18:44:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# In-memory data store
+
 books = []
 
-# Home route to display books
+
 @app.route('/')
 def index():
     return render_template('index.html', books=books)
 
-# Route to add a book
+
 @app.route('/add', methods=['POST'])
 def add_book():
     title = request.form.get('title')
@@ -19,7 +19,7 @@ def add_book():
         books.append({"title": title, "author": author})
     return redirect(url_for('index'))
 
-# Route to delete a book by title
+
 @app.route('/delete/<string:title>', methods=['POST'])
 def delete_book(title):
     global books
